ee957a845be343db47eef8f152170a48aea863a1
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2025-01-22 11:47:23 +0100

    initial

---
+++ BeestjeOpJeFeestje_PROG6/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BeestjeOpJeFeestje_PROG6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
