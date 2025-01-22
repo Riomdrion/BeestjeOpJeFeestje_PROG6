93f1c82b86b5c63279c52d3e5b72b1ba4d658cdb
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2025-01-22 12:14:08 +0100

    unittest project

---
+++ BeestjeOpJeFeestje_PROG6.unitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BeestjeOpJeFeestje_PROG6.unitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
